f4db8715f12a6ebe3f65bc08faa8ee09afc0ec5f
Amara Vo <61066386+av2jf@users.noreply.github.com>
2023-03-26 11:10:41 -0400

    Update Product.js

--- react-app/src/pages/Product/Product.js
+++ react-app/src/pages/Product/Product.js
@@ -1,13 +1,3 @@
-import { withStyles } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import Button from '@material-ui/core/Button';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
